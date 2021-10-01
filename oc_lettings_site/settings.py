f0d1e16235dd3ab0acb565252e5a55ca3dc809ab
FortranVBA <mpyfabrice@gmail.com>
2021-10-01 23:44:54 +0200

    Mise en place de gunicorn

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*", "192.168.99.100"]
+ALLOWED_HOSTS = ["192.168.99.100"]
 
 
 # Application definition
