83dd604e08a98989c7af0defeefd9ca98a644123
FortranVBA <mpyfabrice@gmail.com>
2021-10-06 11:38:02 +0200

    Ajout d'un host

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,7 +15,8 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.99.100", "dap13.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.99.100", "dap13.herokuapp.com", "127.0.0.1", 
+                 "oc-lettings-mpyf.herokuapp.com"]
 
 
 # Application definition
